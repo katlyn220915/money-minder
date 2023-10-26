9f602a4898f12101df8f1907c35c2155bad0da1a
Katlyn <katlyn880815@gmail.com>
2023-10-26 21:44:06 +0800

    added a hint to user input

--- components/accounting-page/AccountingForm.tsx
+++ components/accounting-page/AccountingForm.tsx
@@ -9,6 +9,7 @@ const AccountingForm: React.FC<AccountingFormProps> = ({ onAddRecord }) => {
   const [bill, setBill] = useState<number>(0);
   const [description, setDescription] = useState<string>("");
   const [isInputClicked, setIsInputClicked] = useState<boolean>(false);
+  const [isNumber, setIsNumber] = useState<boolean>(true);
 
   const handleAddRecord = (
     e: React.MouseEvent<HTMLButtonElement>,
@@ -53,46 +54,52 @@ const AccountingForm: React.FC<AccountingFormProps> = ({ onAddRecord }) => {
   }
 
   return (
-    <form className="accounting__form container-sm">
-      <div className="accounting__form__block">
-        <select
-          value={expenseOrIncome}
-          onChange={(e) => setExpenseOrIncome(e.target.value)}
-          className="input-group"
-        >
-          <option value="expense">支出</option>
-          <option value="income">收入</option>
-        </select>
-        <input
-          className={isInputClicked ? billInputClassName : "input-group"}
-          placeholder="請輸入金額"
-          type="text"
-          value={bill}
-          onChange={(e) => {
-            const inputValue = e.target.value;
-            if (/^\d*$/.test(inputValue)) {
-              setBill(Number(inputValue));
-            }
-          }}
-          onClick={handleInputClick}
-          required
-        />
-        <input
-          className={isInputClicked ? desInputClassName : "input-group"}
-          placeholder="請輸入詳細資訊"
-          type="text"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          required
-        />
-        <button
-          className="btn btn__action"
-          onClick={(e) => handleAddRecord(e, bill)}
-        >
-          新增紀錄
-        </button>
-      </div>
-    </form>
+    <>
+      <form className="accounting__form container-sm">
+        <div className="accounting__form__block">
+          <select
+            value={expenseOrIncome}
+            onChange={(e) => setExpenseOrIncome(e.target.value)}
+            className="input-group"
+          >
+            <option value="expense">支出</option>
+            <option value="income">收入</option>
+          </select>
+          <input
+            className={isInputClicked ? billInputClassName : "input-group"}
+            placeholder="請輸入金額"
+            type="text"
+            value={bill}
+            onChange={(e) => {
+              const inputValue = e.target.value;
+              if (/^\d*$/.test(inputValue)) {
+                setIsNumber(true);
+                setBill(Number(inputValue));
+              } else {
+                setIsNumber(false);
+              }
+            }}
+            onClick={handleInputClick}
+            required
+          />
+          <input
+            className={isInputClicked ? desInputClassName : "input-group"}
+            placeholder="請輸入詳細資訊"
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+          <button
+            className="btn btn__action"
+            onClick={(e) => handleAddRecord(e, bill)}
+          >
+            新增紀錄
+          </button>
+        </div>
+      </form>
+      {isNumber ? "" : <p className="hint">請切換數字鍵盤輸入數字！</p>}
+    </>
   );
 };
 
